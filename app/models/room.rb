70381e7f84b91e4dc9abfc59d7526fc455119af9
dennerruby <dennersk891@gmail.com>
2022-07-27 23:33:53 -0300

    add validates and model room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+    validates_presence_of :title, :location
+    validates_length_of :description, minimum: 10, allow_blank: false, :message => "nao pode ficar em branco "
 end
