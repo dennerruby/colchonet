93bebf99d3609c019649ac9965581ef36959dc76
dennerruby <dennersk891@gmail.com>
2022-08-02 08:23:25 -0300

    add bulma css

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
-    validates_presence_of :title, :location
-    validates_length_of :description, minimum: 10, allow_blank: false, :message => "nao pode ficar em branco "
+  validates_presence_of :title, :location
+  validates_length_of :description, minimum: 10, allow_blank: false, :message => "nao pode ficar em branco "
+
+  def complete_name
+    "#{title}, #{location}"
+  end
 end
