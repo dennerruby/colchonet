a99b1fee74ce652a4ba9b442576881010dba2707
Denner <denner@pop-os.localdomain>
2023-01-31 22:41:33 -0300

    Create email confirm user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  resources :rooms
+  resources :users
+
+  resource :confirmation, only: [:show]
+
+
   LOCALES = /en|pt\-BR/
 
   scope :"(locale)", locale: LOCALES do
     resources :rooms, :users
   end
-  
+
   get '/:locale' => 'home#index', locale: LOCALES
   root "home#index"
 end
